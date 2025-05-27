58eb441d17844665f076a40c9d2dbd8d537b9ea0
hazellee0914 <starmagic2206@gmail.com>
2025-05-27 09:03:27 +0900

    sec01

---
+++ Algorithm2/section05/sec01.js
@@ -0,0 +1,22 @@
+/**
+ * 숫자열인 입력데이터 2개를 받아 나누기 기능을 하는 함수를 만들어야 합니다.
+ */
+
+function divide(a, b) {
+  console.log(a / b);
+  return a / b;
+}
+
+divide(10, 2);
+
+const divide2 = function (a, b) {
+  console.log(a / b);
+};
+
+divide2(10, 2);
+
+const divide3 = (a, b) => {
+  console.log(a / b);
+};
+
+divide3(10, 2);
