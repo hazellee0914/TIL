86a579ac448c3dff786883e77cab6f53b9bb191c
hazellee0914 <starmagic2206@gmail.com>
2025-08-17 17:00:27 +0900

    Js-점수계산  코드수정

--- Algorithm3/section02/JS-점수계산.js
+++ Algorithm3/section02/JS-점수계산.js
@@ -1,22 +1,34 @@
 function solution(arr) {
-  const result = arr.reduce(
-    (acc, cur) => {
-      if (cur === 1) {
-        acc.score += 1; // 연속 맞춘 점수 증가
-        acc.sum += acc.score; // 총합에 더하기
-      } else {
-        acc.score = 0; // 틀리면 연속 점수 초기화
-      }
-      return acc;
-    },
-    { sum: 0, score: 0 } // 초기값
-  );
-
-  return result.sum;
+  return arr.reduce(
+    ([sum, score], cur) =>
+      cur === 1 ? [sum + score + 1, score + 1] : [sum, 0],
+    [0, 0]
+  )[0];
 }
 
 let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
-console.log(solution(arr));
+console.log(solution(arr)); // 1
+
+// function solution(arr) {
+//   const result = arr.reduce(
+//     (acc, cur) => {
+//       if (cur === 1) {
+//         acc.score += 1; // 연속 맞춘 점수 증가
+//         acc.sum += acc.score; // 총합에 더하기
+//       } else {
+//         acc.score = 0; // 틀리면 연속 점수 초기화
+//       }
+//       return acc;
+//     },
+//     { sum: 0, score: 0 } // 초기값
+//   );
+
+//   return result.sum;
+// }
+
+// let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+// console.log(solution(arr));
+
 //  function solution(arr){
 //        let sum = 0;
 //        let score = 0;
