1cd23b0380afed1d71ec2c70462823ca3bde2381
hazellee0914 <starmagic2206@gmail.com>
2025-08-17 16:56:40 +0900

    Js-점수계산

---
+++ Algorithm3/section02/JS-점수계산.js
@@ -0,0 +1,37 @@
+function solution(arr) {
+  const result = arr.reduce(
+    (acc, cur) => {
+      if (cur === 1) {
+        acc.score += 1; // 연속 맞춘 점수 증가
+        acc.sum += acc.score; // 총합에 더하기
+      } else {
+        acc.score = 0; // 틀리면 연속 점수 초기화
+      }
+      return acc;
+    },
+    { sum: 0, score: 0 } // 초기값
+  );
+
+  return result.sum;
+}
+
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
+//  function solution(arr){
+//        let sum = 0;
+//        let score = 0;
+
+//        for(let x of arr){
+//         if(x === 1){
+//           score++;
+//         sum+=score
+//                   } else{
+//                     score =0
+//                   }
+
+//        }
+//        return sum
+//         }
+
+//         let arr=[1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+//         console.log(solution(arr));
