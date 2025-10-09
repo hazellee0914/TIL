dd510e42a2a1388cd59732ceeef89eeb7f855e73
hazellee0914 <starmagic2206@gmail.com>
2025-10-09 14:20:07 +0900

    Js-fibo

---
+++ Algorithm3/section08/JS-fibo.js
@@ -0,0 +1,23 @@
+let input = 100;
+
+const memo = new Map();
+memo.set(1, 1);
+memo.set(2, 1);
+
+// 1. 메모에 이미 해당 값이 있으면 반환한다.
+// 2. 만약 없다면, 그 값을 피보나치를 통해 구하고 메모에 저장한다.
+function fiboDynamicProgramming(n, fiboMemo) {
+  if (fiboMemo.has(n)) {
+    return fiboMemo.get(n);
+  }
+
+  const nthFibo =
+    fiboDynamicProgramming(n - 1, fiboMemo) +
+    fiboDynamicProgramming(n - 2, fiboMemo);
+  fiboMemo.set(n, nthFibo);
+
+  return nthFibo;
+}
+
+// Main execution
+console.log(fiboDynamicProgramming(input, memo));
