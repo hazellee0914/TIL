4c3f2b44a8e745dd4912246e86d29d6b82b2fada
hazellee0914 <starmagic2206@gmail.com>
2025-11-10 17:10:44 +0900

    Js-돌다리건너기

---
+++ Algorithm3/section10/JS-돌다리건너기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let res = 0;
+  let dy = Array.from({ length: n + 2 }, () => 0);
+  dy[1] = 1;
+  dy[2] = 2;
+  for (let i = 3; i <= n + 1; i++) {
+    dy[i] = dy[i - 2] + dy[i - 1];
+  }
+  res = dy[n + 1];
+  return res;
+}
+
+console.log(solution(7));
